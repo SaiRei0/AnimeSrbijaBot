52df9276d1b278fe5ebd8dcab0117e7158b9c7c9
Po2kkz <toni.pejic98@gmail.com>
2016-06-08 15:28:49 +0200

    u3

--- AnimeSrbijaBot.js
+++ AnimeSrbijaBot.js
@@ -63,6 +63,12 @@
         for (var prop in obj) {
             chat = chat.replace(lit + prop.toUpperCase() + lit, obj[prop]);
         }
+        chat = chat.substring(4);
+		while(chat.startsWith('!') || chat.startsWith('/'))
+		{
+			chat = chat.substring(1);
+		}
+		chat = "/me " + chat;
         return chat;
     };
 
